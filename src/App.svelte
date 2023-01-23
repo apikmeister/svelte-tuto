5e686445f5506adf5cc26a430538bb21ec86a7c7
apikmeister <afiq.mohamad90@gmail.com>
2023-01-23 18:30:21 +0800

    reactive statement

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,51 @@
 <script>
-	let firstName = 'apik'
-	let lastName = 'Nish'
-	let beltColour = 'black'
+  let firstName = "apik";
+  let lastName = "Nish";
+  let beltColour = "black";
 
-	$: fullName = `${firstName} ${lastName}`
+  $: fullName = `${firstName} ${lastName}`;
+  $: {
+    console.log(beltColour);
+    console.log(fullName);
+  }
 
-	const handleClick = () => {
-		beltColour = 'red'
-	}
+  const handleClick = () => {
+    beltColour = "red";
+  };
 
-	const handleInput = (event) => {
-		beltColour = event.target.value
-	}
+  const handleInput = (event) => {
+    beltColour = event.target.value;
+  };
 </script>
 
 <main>
-	<!-- <h1>Hello {name}!</h1> -->
-	<p>{fullName} - {beltColour} belt</p>
-	<input type="text" bind:value={firstName}>
-	<input type="text" bind:value={lastName}>
-	<!-- <button on:click={handleClick}>update belt colour</button> -->
-	<!-- <input type="text" on:input={handleInput} value={beltColour}> -->
-	<input type="text" bind:value={beltColour}>
+  <!-- <h1>Hello {name}!</h1> -->
+  <p>{fullName} - {beltColour} belt</p>
+  <input type="text" bind:value={firstName} />
+  <input type="text" bind:value={lastName} />
+  <!-- <button on:click={handleClick}>update belt colour</button> -->
+  <!-- <input type="text" on:input={handleInput} value={beltColour}> -->
+  <input type="text" bind:value={beltColour} />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
