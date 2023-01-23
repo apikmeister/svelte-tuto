3f061b69f01c771ffff12ad501988623d5ae0baf
apikmeister <afiq.mohamad90@gmail.com>
2023-01-23 18:21:08 +0800

    realtime css

--- src/App.svelte
+++ src/App.svelte
@@ -13,7 +13,7 @@
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>{beltColour} belt</p>
+	<p style="color: {beltColour}">{beltColour} belt</p>
 	<button on:click={handleClick}>update belt colour</button>
 	<!-- <input type="text" on:input={handleInput} value={beltColour}> -->
 	<input type="text" bind:value={beltColour}>
